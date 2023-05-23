377e33d42857a9b2873ef06957194b8fb3016f0c
Rellopn <614046330@qq.com>
2023-05-23 14:41:44 +0800

    新增两种请求方式

--- yunClient.go
+++ yunClient.go
@@ -65,14 +65,19 @@ func setTlsClient(tlPath string) {
 }
 
 // 加了一个签名源，为页面跳转、异步响应报文验签，签名源串为：sysid + rps + timestamp
-func (y *YunClient) Request(service string, method string, params map[string]interface{}, sourceFrom ...int64) (*http.Response, map[string]string, error) {
-	up, sign, err := y.buildPostBody(map[string]interface{}{"service": service, "method": method, "param": params})
+func (y *YunClient) Request(service string, method string, params map[string]any, sourceFrom ...int64) (*http.Response, map[string]string, error) {
+	nowTime := time.Now().Format("2006-01-02 15:04:05")
+	up, err := y.BuildPostBody(map[string]any{"service": service, "method": method, "param": params}, nowTime)
 	if err != nil {
 		return nil, nil, err
 	}
-	trueUrl := y.serverUrl + up + "sign=" + sign
+	signStr, err := y.Sign1(map[string]any{"service": service, "method": method, "param": params}, nowTime)
+	if err != nil {
+		return nil, nil, err
+	}
+	trueUrl := y.serverUrl + url.PathEscape(up) + "sign=" + signStr
 	if sourceFrom != nil && len(sourceFrom) >= 1 && sourceFrom[0] == 1 {
-		return nil, map[string]string{"toUrl": y.signContactUrl + url.PathEscape(up) + "sign=" + sign}, nil
+		return nil, map[string]string{"toUrl": y.signContactUrl + url.PathEscape(up) + "sign=" + signStr}, nil
 	}
 	resp, err := httpClient.Post(trueUrl, "application/x-www-form-urlencoded;charset=utf-8", nil)
 	if err != nil {
@@ -101,30 +106,81 @@ func (y *YunClient) Request(service string, method string, params map[string]int
 	return resp, res, nil
 }
 
-func (y *YunClient) buildPostBody(params map[string]interface{}) (string, string, error) {
+func (y *YunClient) RequestBody(service string, method string, params map[string]any, sourceFrom ...int64) (*http.Response, map[string]any, error) {
+	nowTime := time.Now().Format("2006-01-02 15:04:05")
+	sendParam := map[string]any{"service": service, "method": method, "param": params}
+	signStr, err := y.Sign1(sendParam, nowTime)
+	if err != nil {
+		return nil, nil, err
+	}
+	sendParam["sign"] = signStr
+	if sourceFrom != nil && len(sourceFrom) >= 1 && sourceFrom[0] == 1 {
+		return nil, sendParam, nil
+	}
+	pBytes, err := json.Marshal(&sendParam)
+	if err != nil {
+		return nil, nil, err
+	}
+	resp, err := httpClient.Post(y.serverUrl, "application/json;charset=utf-8", strings.NewReader(string(pBytes)))
+	//resp, err := httpClient.Post(y.serverUrl, "application/x-www-form-urlencoded;charset=utf-8", nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	var res map[string]string
+	responseBodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, nil, err
+	}
+	err = json.Unmarshal(responseBodyBytes, &res)
+	if err != nil {
+		return nil, nil, err
+	}
+	// 默认是同步请求
+	//if sourceFrom == nil || len(sourceFrom) == 0 {
+	if err := verifySign1(res); err != nil {
+		return nil, nil, err
+		//}
+		//} else { //页面跳转、异步响应报文验签
+		//	if err := verifySign2(res); err != nil {
+		//		return nil, nil, err
+		//	}
+	}
+
+	var rt = make(map[string]any)
+	for k, v := range res {
+		rt[k] = v
+	}
+	return resp, rt, nil
+}
+
+func (y *YunClient) BuildPostBody(params map[string]any, nowTime string) (string, error) {
 	pBytes, err := json.Marshal(&params)
 	if err != nil {
-		return "", "", err
-	}
-	enCodeParams := url.QueryEscape(string(pBytes))
-	timestamp := time.Now().Format("2006-01-02 15:04:05")
-	encodeTimestamp := url.QueryEscape(timestamp)
-	if err != nil {
-		return "", "", err
+		return "", err
 	}
 	sb := strings.Builder{}
 	sb.WriteString("sysid=")
 	sb.WriteString(y.sysId)
 	sb.WriteString("&timestamp=")
-	sb.WriteString(encodeTimestamp)
+	sb.WriteString(nowTime)
 	sb.WriteString("&v=")
 	sb.WriteString(y.version)
 	sb.WriteString("&req=")
-	sb.WriteString(enCodeParams)
+	sb.WriteString(string(pBytes))
 	sb.WriteString("&")
-	sign, err := Sign(y.sysId + string(pBytes) + timestamp)
-	enCodeSign := caseTranslate(url.QueryEscape(sign))
-	return sb.String(), enCodeSign, nil
+	return sb.String(), nil
+}
+
+func (y *YunClient) Sign1(params map[string]any, nowTime string) (string, error) {
+	pBytes, err := json.Marshal(&params)
+	if err != nil {
+		return "", err
+	}
+	sign, err := Sign(y.sysId + string(pBytes) + nowTime)
+	if err != nil {
+		return "", err
+	}
+	return caseTranslate(url.QueryEscape(sign)), nil
 }
 
 func caseTranslate(sign string) string {
@@ -163,4 +219,62 @@ func VerifySign2(res map[string]string) error {
 	}
 	willValidateSource := res["sysid"] + res["rps"] + res["timestamp"]
 	return VerifySign(willValidateSource, res["sign"])
+}
+
+type SendPar struct {
+	AppAuthToken string `json:"appAuthToken"`
+	BizContent   any    `json:"bizContent"`
+	Charset      string `json:"charset"`
+	Format       string `json:"format"`
+	Method       string `json:"method"`
+	Sign         string `json:"sign"`
+	SignType     string `json:"signType"`
+	Timestamp    string `json:"timestamp"`
+	Version      string `json:"version"`
+}
+
+func (y *YunClient) Request2(method string, params map[string]any) (*http.Response, map[string]string, error) {
+	nowTime := time.Now().Format("2006-01-02 15:04:05")
+	up, sign, err := y.Sign2(nowTime, method, params)
+	if err != nil {
+		return nil, nil, err
+	}
+	postBody := strings.NewReader(up + "sign=" + sign + "&signType=SHA256WithRSA")
+	resp, err := httpClient.Post(y.serverUrl, "application/x-www-form-urlencoded;charset=utf-8", postBody)
+	if err != nil {
+		return nil, nil, err
+	}
+	var res map[string]string
+	responseBodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, nil, err
+	}
+	err = json.Unmarshal(responseBodyBytes, &res)
+	if err != nil {
+		return nil, nil, err
+	}
+	return resp, res, nil
+}
+
+func (y *YunClient) Sign2(timeNow, method string, params map[string]any) (string, string, error) {
+	pBytes, err := json.Marshal(&params)
+	if err != nil {
+		return "", "", err
+	}
+	//enCodeParams := url.QueryEscape(string(pBytes))
+	sb := strings.Builder{}
+	sb.WriteString(`appId=`)
+	sb.WriteString(y.sysId)
+	sb.WriteString(`&appAuthToken=""&bizContent=`)
+	sb.WriteString(string(pBytes))
+	sb.WriteString(`&charset=utf-8&format=json&method=`)
+	sb.WriteString(method)
+	sb.WriteString(`&timestamp=`)
+	sb.WriteString(timeNow)
+	sb.WriteString(`&version=`)
+	sb.WriteString(y.version)
+	sb.WriteString(`&`)
+	sign, err := Sign(sb.String())
+	enCodeSign := caseTranslate(url.QueryEscape(sign))
+	return sb.String(), enCodeSign, nil
 }
